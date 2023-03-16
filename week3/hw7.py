56ed93766dfa7f37cdb1d84c3f7a4d1437c2508b
shushmanuk <shushanik.manukian@gmail.com>
2023-03-16 21:50:46 +0400

    Homework

--- week3/hw7.py
+++ week3/hw7.py
@@ -1,4 +1,8 @@
 #homework N7 Find out the inputed number is even number or odd number
+#version1
+num = -29
+print("even" * (num % 2 == 0),"odd" * (num % 2 != 0))
+
 #version2
 num = 48
 if(num % 2 == 0):
