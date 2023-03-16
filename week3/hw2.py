48d2a55f986d648820e030c7b58d98f415631bcd
shushmanuk <shushanik.manukian@gmail.com>
2023-03-16 17:55:07 +0400

    Homework

---
+++ week3/hw2.py
@@ -0,0 +1,8 @@
+#homework N2 Checking a repdigit number
+#version1
+num = 33
+print ((num // 10) == (num % 10))
+
+#version2
+num = int(input("Enter a two-digit number, please" ))
+print ((num // 10) == (num % 10))
