72b03bdd4c6d54b11b45e02bb7baf8375765d4b4
shushmanuk <shushanik.manukian@gmail.com>
2023-05-27 15:51:59 +0400

    Update 2

--- week2/lesson3.py
+++ week2/lesson3.py
@@ -1,19 +1,19 @@
-"Beautiful is better than ugly."
-"Explicit is better than implicit."
-"Simple is better than complex."
-"Complex is better than complicated."
-"Flat is better than nested."
-"Sparse is better than dense."
-"Readability counts."
-"Special cases aren't special enough to break the rules."
-"Although practicality beats purity."
-"Errors should never pass silently."
-"Unless explicitly silenced."
-"In the face of ambiguity, refuse the temptation to guess."
-"There should be one-- and preferably only one --obvious way to do it."
-"Although that way may not be obvious at first unless you're Dutch."
-"Now is better than never."
-"Although never is often better than *right* now."
-"If the implementation is hard to explain, it's a bad idea."
-"If the implementation is easy to explain, it may be a good idea."
-"Namespaces are one honking great idea -- let's do "
+"Beautiful is better than ugly." #Գեղեցիկն ավելի լավ է քան տգեղը։
+"Explicit is better than implicit." #Բացահայտն ավելի լավ է քան ծածուկը։
+"Simple is better than complex." #Պարզն ավելի լավ է քան բարդը։
+"Complex is better than complicated." #Բարդն ավելի լավ է քան խճճվածը։
+"Flat is better than nested." #Հարթն ավելի լավ է քան բնադրվածը։
+"Sparse is better than dense." #Նոսրն ավելի լավ է քան խիտը։
+"Readability counts." #Ընթեռնելիությունը հաշվվում է։
+"Special cases aren't special enough to break the rules." #Հատուկ դեպքերն այնքան հատուկ չեն, որ խախտեն կանոնները։
+"Although practicality beats purity." #Չնայած գործնականությունը հաղթում է անթերիությանը։
+"Errors should never pass silently." #Սխալները երբեք չպետք է անտեսվեն։
+"Unless explicitly silenced." #Հակառակ այն բանի, որ հստակությունը լռեցված է։
+"In the face of ambiguity, refuse the temptation to guess." #Անորոշության հանդիպելիս, խուսափիր գուշակելու գայթակղությունից։
+"There should be one-- and preferably only one --obvious way to do it." #Կա մեկ և, հավանաբար,միակ ճանապարհ անելու դա։
+"Although that way may not be obvious at first unless you're Dutch." #Չնայած դա հազիվ թե ակնհայտ լինի, եթե, իհարկե, դուք հոլանդացի չեք։
+"Now is better than never." #Ավելի լավ է հիմա, քան երբեք։
+"Although never is often better than *right* now." #Չնայած երբեք֊ը հաճախ ավելի լավ է քան հենց հիմա֊ն։
+"If the implementation is hard to explain, it's a bad idea." #Եթե իրականացումը դժվար է բացատրել, ուրեմն դա վատ միտք է։
+"If the implementation is easy to explain, it may be a good idea." #Եթե իրականացումը դժվար է բացատրել, ուրեմն այն կարող է լավ միտք լինել։
+"Namespaces are one honking great idea -- let's do " #Բացատներ դնելը միաձայն հրաշալի գաղափար է, եկեք անենք։
