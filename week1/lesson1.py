1d801b85e53f772a9b454438ea5efcd743ffc241
shushmanuk <shushanik.manukian@gmail.com>
2023-05-27 15:51:14 +0400

    Update

--- week1/lesson1.py
+++ week1/lesson1.py
@@ -1,1 +1,4 @@
-print("Hi, there!")
+print("Hello, world!")
+
+text = "This is my first message to future me, \nwhen I'm going to work as a Python programmer."
+print(text)
--- week1/lesson2.py
+++ week1/lesson2.py
@@ -1,2 +1,7 @@
-print("What about you?")
-print(2 + 3)
+step1 = "Work hard."
+step2 = "Be patient."
+step3 = "Do whatever you want."
+step4 = "Remember your dream! \nThis is your dream to be a programmer \nand it's what you need to work for."
+p = f"1. {step1} \n2. {step2} \n3. {step3} \n4. {step4}"
+print(p)
+print(type(p))
